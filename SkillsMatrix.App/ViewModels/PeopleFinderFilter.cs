c28922b21fcc9b6406934b51514d051a651fcf6c
Ekrigh <Erik.krigh@hotmail.com>
2024-08-21 12:58:53 +0200

    added peoplefinder filter button and component

---
+++ SkillsMatrix.App/ViewModels/PeopleFinderFilter.cs
@@ -0,0 +1,10 @@
+﻿using SkillsMatrix.Infrastructure.Models;
+
+namespace SkillsMatrix.App.ViewModels
+{
+    public class PeopleFinderFilter
+    {
+        public Skill? Skill { get; set; }
+        public int Rating { get; set; } = 1;
+    }
+}
